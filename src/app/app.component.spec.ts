91975942415ea5da406dafc9a4844c69b99e7076
inyps1 <suraj.lama109@gmail.com>
2020-03-21 15:25:54 -0400

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('myapp2');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('myapp2 app is running!');
-  });
 });
